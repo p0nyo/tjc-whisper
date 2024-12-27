4f31c5fba7b3b0066ffc1bbbf25e9c2161c1e511
AV Team TJC Auckland <av@akl.tjc.org.nz>
2024-12-27 20:50:58 +1300

    fix: fine tuning

--- speech-to-text/__main__.py
+++ speech-to-text/__main__.py
@@ -19,7 +19,14 @@ thread: threading.Thread = None
 def start_transcription():
     global transcriber, event_loop, thread
     try:
-        filtered_app_settings = {'audio_device': 0, 'silence_limit': 8, 'noise_threshold': 5, 'non_speech_threshold': 0.1, 'include_non_speech': False, 'create_audio_file': False, 'use_websocket_server': False, 'use_openai_api': False}
+        filtered_app_settings = {'audio_device': 0, 
+                                 'silence_limit': 1, 
+                                 'noise_threshold': 9, 
+                                 'non_speech_threshold': 0.1, 
+                                 'include_non_speech': False, 
+                                 'create_audio_file': False, 
+                                 'use_websocket_server': False, 
+                                 'use_openai_api': False}
         # Model Options: 
         # tiny, tiny.en, base, base.en, small, small.en, medium
         # medium.en, large-v1, large-v2, large-v3, distil-large-v2, distil-medium.en
@@ -29,8 +36,36 @@ def start_transcription():
         # cpu, cuda, auto
         # cuda is gpu
         # need to download cuda if running it
-        filtered_model_settings = {'model_size_or_path': 'tiny', 'device': 'cpu', 'device_index': 0, 'compute_type': 'default', 'cpu_threads': 0, 'num_workers': 1, 'local_files_only': False}
-        filtered_transcribe_settings = {'language': 'en', 'task': 'transcribe', 'beam_size': 5, 'best_of': 5, 'patience': 1, 'length_penalty': 1, 'repetition_penalty': 1, 'no_repeat_ngram_size': 0, 'temperature': [0, 0.2, 0.4, 0.6, 0.8, 1], 'compression_ratio_threshold': 2.4, 'log_prob_threshold': -1, 'no_speech_threshold': 0.6, 'condition_on_previous_text': True, 'suppress_blank': True, 'suppress_tokens': [-1], 'without_timestamps': False, 'max_initial_timestamp': 1, 'word_timestamps': False, 'prepend_punctuations': '\\"\'“¿([{-', 'append_punctuations': '\\"\'.。,，!！?？:：”)]}、', 'vad_filter': False, 'vad_parameters': {'threshold': 0.5, 'min_speech_duration_ms': 250, 'max_speech_duration_s': 0, 'min_silence_duration_ms': 2000, 'speech_pad_ms': 400}}
+        filtered_model_settings = {'model_size_or_path': 'large-v2', 
+                                   'device': 'cuda', 
+                                   'device_index': 0, 
+                                   'compute_type': 'default', 
+                                   'cpu_threads': 0, 
+                                   'num_workers': 1, 
+                                   'local_files_only': False}
+        filtered_transcribe_settings = {'language': 'en', 
+                                        'task': 'transcribe', 
+                                        'beam_size': 5, 
+                                        'best_of': 5, 
+                                        'patience': 1, 
+                                        'length_penalty': 1, 
+                                        'repetition_penalty': 1, 
+                                        'no_repeat_ngram_size': 0, 
+                                        'temperature': [0, 0.2, 0.4, 0.6, 0.8, 1], 
+                                        'compression_ratio_threshold': 2.4, 
+                                        'log_prob_threshold': -1, 
+                                        'no_speech_threshold': 0.6, 
+                                        'condition_on_previous_text': True, 
+                                        'suppress_blank': True, 
+                                        'suppress_tokens': [-1], 
+                                        'without_timestamps': False, 
+                                        'max_initial_timestamp': 1, 
+                                        'word_timestamps': False, 
+                                        'multilingual': False,
+                                        'prepend_punctuations': '\\"\'“¿([{-', 
+                                        'append_punctuations': '\\"\'.。,，!！?？:：”)]}、', 
+                                        'vad_filter': False, 
+                                        'vad_parameters': {'threshold': 0.5, 'min_speech_duration_ms': 250, 'max_speech_duration_s': 0, 'min_silence_duration_ms': 2000, 'speech_pad_ms': 400}}
         
         whisper_model = WhisperModel(**filtered_model_settings)
         app_settings = AppOptions(**filtered_app_settings)
